e2a5c4a74cedd600adc1220963d6384acbcd0faa
choichoi463 <choichoi463@gmail.com>
2019-05-27 22:36:36 +0200

    hooray, specflow 3 works now woth nunit runner. See guide how to setup this shit. Step Definitions are  in pending for now

---
+++ Framework3/ContactUs.feature
@@ -0,0 +1,10 @@
+﻿Feature: ContactUs page loading
+	In order to contact us 
+	As a website user
+	I want to be able to navigate to ContactUs page
+
+@sanity
+Scenario: Open ContactUs page
+	Given I am on start page of blog.io
+	When I click on Contact Us menu item
+	Then User is redirected to ContactUs page
---
+++ Framework3/StepDefinitions/ContactUsPageLoadingSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Framework3.StepDefinitions
+{
+    [Binding]
+    public class ContactUsPageLoadingSteps
+    {
+        [Given(@"I am on start page of blog\.io")]
+        public void GivenIAmOnStartPageOfBlog_Io()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on Contact Us menu item")]
+        public void WhenIClickOnContactUsMenuItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User is redirected to ContactUs page")]
+        public void ThenUserIsRedirectedToContactUsPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
